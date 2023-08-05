faa00dea42e9485fdb74b08a44c7f185a08aab6c
Rui Lagos <ruifilipelagos@gmail.com>
2023-08-06 00:29:09 +0100

    Adding the cast vote and query Ethers from Hardhat

--- scripts/DeployWithHardhat.ts
+++ scripts/DeployWithHardhat.ts
@@ -37,19 +37,61 @@ async function main() {
       
     };
 
-    try {
-      //hardcode an wallet to pass as string, executing the function giveRigthToVote
+    
+    console.log('Contract finally deployed');
+    console.log('Rigth now lets use the contract');
+    console.log('----------------------------------------------------------------------------------------');
+    console.log('----------------------------------------------------------------------------------------');
+    console.log('EXECUTING : GIVE RIGHTS TO OTHER ADRESS');
+    
       //i am using ether to give me the secound adrees to use 
       const accounts = await ethers.getSigners();
       const voter = accounts[1].address;
 
-
+      //then use the wallet to pass as string, executing the function giveRigthToVote
       const passouVotes = await ballotContract.giveRightToVote(voter);
       await passouVotes.wait();
       console.log(`Voting right given to ${voter}`);
-    } catch (error) {
-      console.error(`Error giving voting right: ${error}`);
-    }
+   
+
+    console.log('----------------------------------------------------------------------------------------');
+    console.log('----------------------------------------------------------------------------------------');
+    console.log('EXECUTING : Cast Vote');
+
+    
+      // give a random number to vote on that proposal 
+        const proposalIndex: number = Math.floor(Math.random() * 3); 
+        const votedIndex = await ballotContract.vote(proposalIndex);
+        await votedIndex.wait();
+        console.log(`Vote casted for proposal ${proposalIndex}`);
+     
+    //console.log('----------------------------------------------------------------------------------------');
+    //console.log('----------------------------------------------------------------------------------------');
+    //console.log('EXECUTING : Delegate Vote');
+
+    
+       // const delegateVote = await ballotContract.delegate(voter);
+       // await delegateVote.wait();
+       // console.log(`Vote delegated to ${voter}`);
+      
+    console.log('----------------------------------------------------------------------------------------');
+    console.log('----------------------------------------------------------------------------------------');
+    console.log('EXECUTING : QUERY');
+
+  
+        const winningProposal = await ballotContract.winningProposal();
+        console.log(`Winning proposal index: ${winningProposal}`);
+ 
+
+    
+
+        const winnerName = await ballotContract.winnerName();
+        console.log(`Winner name: ${ethers.decodeBytes32String(winnerName)}`);
+        //console.log(`Winner name: ${ethers.utils.parseBytes32String(winnerName)}`);
+
+    
+
+
 
 
     
